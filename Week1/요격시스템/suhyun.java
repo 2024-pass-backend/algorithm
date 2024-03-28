5e61de55aea9adfaf432f06077034343ce0a1f9d
suhyun <qkrtngus116@naver.com>
2024-03-28 19:05:18 +0900

    feat:그리디

--- Week1/요격시스템/suhyun.java
+++ Week1/요격시스템/suhyun.java
@@ -1,4 +1,20 @@
 package Week1.요격시스템;
 
+import java.util.Arrays;
+
 public class suhyun {
+    public int solution(int[][] targets) {
+        int answer = 0;
+
+        Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
+
+        int before = 0;
+        for(int i=0; i<targets.length; i++){
+            if(before <= targets[i][0]){
+                before = targets[i][1];
+                answer++;
+            }
+        }
+        return answer;
+    }
 }
