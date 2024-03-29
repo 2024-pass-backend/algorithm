8a23230de2325693833dee06586dbca2d6262ac1
zzzyoonnn <dwsrh03@gmail.com>
2024-03-29 16:04:02 +0900

    type1 : [Solved] PRG_추억점수

---
+++ Week1/공통/추억점수/jiyoon.java
@@ -0,0 +1,25 @@
+package Week1.공통.추억점수;
+// 추억 점수
+
+public class jiyoon {
+  public int[] solution(String[] park, String[] routes) {
+    int[] answer = new int[photo.length];
+
+    HashMap<String, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < name.length; i++) {
+      map.put(name[i], yearning[i]);
+    }
+
+    for (int i = 0; i < photo.length; i++) {
+      int score = 0;
+      for (int j = 0; j < photo[i].length; j++) {
+        if (map.get(photo[i][j]) == null) continue;
+
+        score += map.get(photo[i][j]);
+      }
+      answer[i] = score;
+    }
+    return answer;
+  }
+}
