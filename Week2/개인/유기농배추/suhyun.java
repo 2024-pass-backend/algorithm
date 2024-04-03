f5fb9fa2accd2dd6164a2a197df0f620d628f686
suhyun <qkrtngus116@naver.com>
2024-04-03 14:27:01 +0900

    feat:그래프탐색

--- Week2/개인/유기농배추/suhyun.java
+++ Week2/개인/유기농배추/suhyun.java
@@ -1,4 +1,55 @@
 package Week2.개인.유기농배추;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class suhyun {
+
+    static int t, m, n, k, a, b;
+    static StringTokenizer st;
+    static int[][] graph;
+    static int[][] dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        t = Integer.parseInt(br.readLine());
+        for(int i=0; i<t; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            graph = new int[n][m];
+            for(int j=0; j<k; j++){
+                st = new StringTokenizer(br.readLine(), " ");
+                a = Integer.parseInt(st.nextToken());
+                b = Integer.parseInt(st.nextToken());
+                graph[b][a] = 1;
+            }
+            int ans = 0;
+            for(int x=0; x<graph.length; x++){
+                for(int y=0; y<graph[x].length; y++){
+                    if(graph[x][y] == 1){
+                        dfs(x, y);
+                        ans += 1;
+                    }
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+
+    static void dfs(int x, int y){
+        graph[x][y] = 0;
+        for(int i=0; i<4; i++){
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m){
+                continue;
+            }
+            if(graph[nx][ny] == 1){
+                dfs(nx, ny);
+            }
+        }
+    }
 }
--- Week2/개인/유기농배추/suhyun1.py
+++ Week2/개인/유기농배추/suhyun1.py
@@ -1,32 +1,35 @@
 import sys
-si = sys.stdin.readline
 sys.setrecursionlimit(100000)
-
+si = sys.stdin.readline
+t = int(si())
 dir = [[0,1], [1,0], [0,-1], [-1,0]]
 
-def dfs(x, y, graph, m, n):
+def dfs(x, y):
     graph[x][y] = 0
+    
     for i in range(4):
         nx = x + dir[i][0]
         ny = y + dir[i][1]
+        
         if nx < 0 or ny < 0 or nx >= n or ny >= m:
             continue
         if graph[nx][ny] == 1:
-            dfs(nx, ny, graph, m, n)
-            
-t = int(si())
+            dfs(nx, ny)
+    
 
-for _ in range(t):
-    answer = 0
+for i in range(t):
     m, n, k = map(int, si().split())
     graph = [[0] * m for _ in range(n)]
-    for _ in range(k):
+    for j in range(k):
         y, x = map(int, si().split())
         graph[x][y] = 1
-    for i, row in enumerate(graph):
-        for j, col in enumerate(row):
-            if col == 1:
-                dfs(i, j, graph, m, n)
-                # print(graph)
-                answer += 1
-    print(answer)
+    
+    ans = 0
+    for a, row in enumerate(graph):
+        for b, col in enumerate(row):
+            if graph[a][b] == 1:
+                dfs(a, b)
+                # print("a = " + str(a) + ", b = " + str(b))
+                ans += 1
+            # print(graph)
+    print(ans)
