333c4517e5f5fc557b3c22642bf44ecd1ded0275
zzzyoonnn <dwsrh03@gmail.com>
2024-04-06 19:13:54 +0900

    edit code

--- Week1/공통/달리기경주/jiyoon.java
+++ Week1/공통/달리기경주/jiyoon.java
@@ -1,30 +1,27 @@
-package Week1.공통.달리기경주;
-// 달리기 경주
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
-class jiyoon {
-    public String[] solution(String[] players, String[] callings) {
+class Solution {
+  public String[] solution(String[] players, String[] callings) {
+    HashMap<String, Integer> rank = new HashMap<>();        // Key : name, Value : rank
 
-    HashMap<String, Integer> map = new HashMap<>();     // Key : 선수 이름, Value : 등수
+    for (int i = 0; i < players.length; i++) rank.put(players[i], i);
 
-    for (int i = 0; i < players.length; i++) map.put(players[i], i);
+    for (int i = 0; i < callings.length; i++) {
+      String name = callings[i];          // 추월한 선수
+      int nowIndex = rank.get(name);      // 추월한 선수의 현재 등수
+      int preIndex = nowIndex - 1;        // 추월당한 선수의 현재 등수
 
-    for (String s : callings) {
-      int ahead = map.get(s);       // 추월하는 선수
-      int behind = map.get(s) - 1;  // 추월당하는 선수
+      String preName = players[preIndex]; // 추월당한 선수
 
-      String outrun = players[ahead];
-      String temp = players[behind];
+      // rank맵 수정
+      rank.replace(name, preIndex);
+      rank.replace(preName, nowIndex);
 
-      players[behind] = outrun;
-      players[ahead] = temp;
-
-      // 선수들의 등수를 변경
-      map.put(outrun, behind);
-      map.put(temp, ahead);
+      // players배열 수정
+      players[preIndex] = name;
+      players[nowIndex] = preName;
     }
 
-    System.out.println(Arrays.toString(players));
+    return players;
   }
 }
