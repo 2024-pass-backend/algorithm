99a5184841dfe30932a5110de0459ee31c0ccec7
0csong <nickyoungdo@gmail.com>
2024-04-09 09:59:29 +0900

    [Solved] PRG_뒤에있는큰수찾기

---
+++ Week3/공통/뒤에있는큰수찾기/youngdo.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    stack = []
+    answer = [-1] * len(numbers)
+
+    for i, num in enumerate(numbers):
+        while stack and numbers[stack[-1]] < num:
+            idx = stack.pop()
+            answer[idx] = num
+        stack.append(i)
+
+    return answer
