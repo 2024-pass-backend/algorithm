76a17966110e681dc61dc7ea44fc1508faafcb23
zzzyoonnn <dwsrh03@gmail.com>
2024-04-12 14:12:44 +0900

    [Solved] PRG_시소 짝꿍(복습)

--- Week3/공통/시소짝꿍/jiyoon.java
+++ Week3/공통/시소짝꿍/jiyoon.java
@@ -2,18 +2,17 @@ package Week3.공통.시소짝꿍;
 import java.util.*;
 
 public class jiyoon {
-
   class Solution {
     public long solution(int[] weights) {
       long answer = 0;
-      HashMap<Double, Integer> map = new HashMap<>();
+      Arrays.sort(weights);
 
-      Arrays.sort(weights);       // weights 배열 정렬
+      HashMap<Double, Integer> map = new HashMap<>();
       for (int i : weights) {
-        double a = i * 1.0;             // Double형태로 변환
-        double b = (i * 2.0) / 3.0;     // 2 : 3의 경우
-        double c = (i * 2.0) / 4.0;     // 2 : 4의 경우
-        double d = (i * 3.0) / 4.0;     // 3 : 4의 경우
+        double a = i * 1.0;
+        double b = (i * 2.0) / 3.0;
+        double c = (i * 1.0) / 2.0;
+        double d = (i * 3.0) / 4.0;
 
         if (map.containsKey(a)) answer += map.get(a);
         if (map.containsKey(b)) answer += map.get(b);
