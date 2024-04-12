146f5479382964da3068c8dfff2d063cdd1a334f
Yubin Han <76162287+yubrainn@users.noreply.github.com>
2024-04-12 09:14:17 +0900

    feat:구현(영도님 스타일대로)

--- Week3/공통/마법의엘리베이터/yubin.py
+++ Week3/공통/마법의엘리베이터/yubin.py
@@ -1,32 +1,12 @@
 def solution(storey):
     answer = 0
-    # 10^c 버튼
     max_c = len(str(storey))
-    for c in range(1, max_c+2):
-        if storey == 0:
-            break
-            
-        now = storey%(10**c)//(10**(c-1))
-        # print(now)
-        # 5미만: 내려가기
-        if now < 5:
-            answer += now
-            storey -= now*(10**(c-1))
-            # print('down', storey)
-        # 5초과: 10-now만큼 올라가기
-        elif now > 5:
-            answer += 10-now
-            storey += (10-now)*(10**(c-1))
-            # print('up', storey)
-        # 5
+    for _ in range(max_c+1):
+        now = storey % 10
+        if (now > 5) or (now==5 and storey//10%10 >= 5):
+            answer += (10-now)
+            storey += (10-now)
         else:
-            answer += 5
-            # 앞 자리수가 5이상이면 올라가기
-            if storey%(10**(c+1))//(10**c) >= 5:
-                storey += 5*(10**(c-1))
-            # 앞 자리수가 5미만이면 내려가기
-            else:
-                storey -= 5*(10**(c-1))
-            # print('5', storey)
-            
+            answer += now
+        storey //= 10
     return answer
