d7df869472f1e11147c705e4fbd0d1e7f181d41b
Yubin Han <76162287+yubrainn@users.noreply.github.com>
2024-04-12 09:13:27 +0900

    feat:구현

--- Week3/공통/마법의엘리베이터/yubin.py
+++ Week3/공통/마법의엘리베이터/yubin.py
@@ -1,31 +1,32 @@
 def solution(storey):
     answer = 0
-    str_storey = str(storey)
-    # 일의 자리 수는 별도 계산
-    for i in range(len(str_storey)-1):
-        now_num = int(str_storey[i])
-        # 현재 내려갈 예정
-        if now_num > 5:
-            answer += 10-now_num
-            # 만약 앞에서 하나 더 못 더하면 지금 더 해주기 
-            if i==0:
-                answer += 1
-            # 뒷 자리에서도 내려갈 예정
-            if int(str_storey[i+1])>5:
-                answer -= 1 # 하나 덜 내려가기
-        # 현재는 올라갈 예정
+    # 10^c 버튼
+    max_c = len(str(storey))
+    for c in range(1, max_c+2):
+        if storey == 0:
+            break
+            
+        now = storey%(10**c)//(10**(c-1))
+        # print(now)
+        # 5미만: 내려가기
+        if now < 5:
+            answer += now
+            storey -= now*(10**(c-1))
+            # print('down', storey)
+        # 5초과: 10-now만큼 올라가기
+        elif now > 5:
+            answer += 10-now
+            storey += (10-now)*(10**(c-1))
+            # print('up', storey)
+        # 5
         else:
-            answer += now_num
-            # 뒷자리에서 내려갈 예정
-            if int(str_storey[i+1])>5:
-                answer += 1
-        
-    # 일의 자리
-    last_num = int(str_storey[-1])
-    # 5보다 크면 내려오기
-    if last_num>5:
-        answer += 10-last_num
-    # 5 같거나 작으면 올라가기
-    else:
-        answer += last_num
+            answer += 5
+            # 앞 자리수가 5이상이면 올라가기
+            if storey%(10**(c+1))//(10**c) >= 5:
+                storey += 5*(10**(c-1))
+            # 앞 자리수가 5미만이면 내려가기
+            else:
+                storey -= 5*(10**(c-1))
+            # print('5', storey)
+            
     return answer
