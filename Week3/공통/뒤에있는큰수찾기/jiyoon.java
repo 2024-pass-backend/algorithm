a41be1b7ea10e8405ebbbc0f6c63c2258f025f56
zzzyoonnn <dwsrh03@gmail.com>
2024-04-12 11:09:17 +0900

    [Solved] PRG_뒤에 있는 큰 수 찾기(복습)

--- Week3/공통/뒤에있는큰수찾기/jiyoon.java
+++ Week3/공통/뒤에있는큰수찾기/jiyoon.java
@@ -5,19 +5,15 @@ public class jiyoon {
   class Solution {
     public int[] solution(int[] numbers) {
       int[] answer = new int[numbers.length];
-      Arrays.fill(answer, -1);        // answer배열을 -1로 채움
-
+      Arrays.fill(answer, -1);
       Stack<Integer> stack = new Stack<>();
 
       for (int i = 0; i < numbers.length; i++) {
-        // 스택이 비어있지 않으면서 현재 스택의 값보다 number의 값이 크면
-        while (!stack.isEmpty() && numbers[i] > numbers[stack.peek()]) {
-          // 뒤에 있는 큰 수에 해당하는 값 pop
+        while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
           answer[stack.pop()] = numbers[i];
         }
         stack.push(i);
       }
-
       return answer;
     }
   }
