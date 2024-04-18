abebce4be7488be7a3cf9e46781bca89b0dd287a
suhyun <qkrtngus116@naver.com>
2024-04-18 13:21:44 +0900

    feat:구현

---
+++ Week4/공통/귤고르기/suhyun.py
@@ -0,0 +1,27 @@
+def solution(k, tangerine):
+    answer = 0
+    
+    a = [0] * 100001
+    # 1 - 1개
+    # 2 - 2개
+    # 3 - 2개
+    # 4 - 1개
+    # 5 - 2개
+    
+    for t in tangerine:
+        a[t] += 1
+        
+    a.sort(reverse=True)
+    c = 0
+    
+    for aa in a:
+        if c < k:
+            c += aa
+        else:
+            break
+        answer += 1
+        
+    return answer
+
+tangerine = [1, 3, 2, 5, 4, 5, 2, 3]
+print(solution(6, tangerine))
