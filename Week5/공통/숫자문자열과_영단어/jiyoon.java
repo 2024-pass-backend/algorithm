23fda307c66eabf3ce06267b45879c50382f238f
zzzyoonnn <dwsrh03@gmail.com>
2024-04-22 10:56:19 +0900

    feat:문자열

---
+++ Week5/공통/숫자문자열과_영단어/jiyoon.java
@@ -0,0 +1,20 @@
+package Week5.공통.숫자문자열과_영단어;
+
+public class jiyoon {
+  class Solution {
+    public int solution(String s) {
+      int answer = 0;
+      String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+      for (int i = 0; i < words.length; i++) {
+        if (s.contains(words[i])) {                                 // s에 영단어 문자가 있다면
+          s = s.replaceAll(words[i], Integer.toString(i));        // 해당 문자열을 숫자로 변경
+        }
+      }
+
+      answer = Integer.parseInt(s);
+
+      return answer;
+    }
+  }
+}
