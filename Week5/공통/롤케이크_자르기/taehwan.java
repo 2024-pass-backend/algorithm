0fccba94ebe3c84f9fd7cc9258ecc6a60303838e
hwan2-99 <thh1233123@gmail.com>
2024-04-23 11:03:30 +0900

    feat: map

---
+++ Week5/공통/롤케이크_자르기/taehwan.java
@@ -0,0 +1,28 @@
+package Week5.공통.롤케이크_자르기;
+import java.util.*;
+
+public class taehwan {public int solution(int[] topping) {
+    int answer = 0;
+
+    Map<Integer, Integer> map_1 = new HashMap<>();
+    Map<Integer, Integer> map_2 = new HashMap<>();
+
+    for (int e : topping) {
+        map_2.put(e, map_2.getOrDefault(e, 0) + 1);
+    }
+
+    for (int e : topping) {
+        map_1.put(e, map_1.getOrDefault(e, 0) + 1);
+
+        if (map_2.get(e) - 1 == 0)
+            map_2.remove(e);
+        else
+            map_2.put(e, map_2.get(e) - 1);
+
+        if (map_1.size() == map_2.size())
+            answer++;
+    }
+
+    return answer;
+}
+}
