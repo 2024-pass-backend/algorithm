d6a2c9e5ac9f3dc1662652afe608808ed541b987
suhyun <qkrtngus116@naver.com>
2024-05-14 12:02:39 +0900

    feat:구현

---
+++ Week7/공통/다른비트_2개이하로/suhyun1.py
@@ -0,0 +1,23 @@
+def solution(numbers):
+    answer = []
+    
+    # for n in range(1, 11):
+    #     print(bin(n)[2:])
+    for n in numbers:
+        if n % 2 == 0:
+            answer.append(n + 1)
+        else:
+            a = '0' + bin(n)[2:]
+            print(a)
+            right_zero_idx = a.rfind('0')
+            t = list(a)
+            t[right_zero_idx] = '1'
+            t[right_zero_idx + 1] = '0'
+            
+            ans_str = ''.join(t)
+            
+            ans = int(ans_str, 2)
+            answer.append(ans)
+    return answer
+
+print(solution([7]))
