05f673476c4bbb73291369c26834ecbd0d8266e1
zzzyoonnn <dwsrh03@gmail.com>
2024-05-29 10:44:49 +0900

    feat: 구현

--- Week10/공통/서울에서김서방찾기/jiyoon.java
+++ Week10/공통/서울에서김서방찾기/jiyoon.java
@@ -1,4 +1,14 @@
 package Week10.공통.서울에서김서방찾기;
 
 public class jiyoon {
+  class Solution {
+    public String solution(String[] seoul) {
+      String answer = "김서방은 ";
+      for (int i = 0; i < seoul.length; i++) {
+        if (seoul[i].equals("Kim")) answer += i;
+      }
+      answer += "에 있다";
+      return answer;
+    }
+  }
 }
--- Week10/공통/수박수박수박수박수박수/jiyoon.java
+++ Week10/공통/수박수박수박수박수박수/jiyoon.java
@@ -1,4 +1,12 @@
 package Week10.공통.수박수박수박수박수박수;
 
 public class jiyoon {
+  class Solution {
+    public String solution(int n) {
+      String answer = "";
+      for (int i = 0; i < n / 2; i++) answer +="수박";
+      if (n % 2 == 1) answer += "수";
+      return answer;
+    }
+  }
 }
