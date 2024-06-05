4d225b3457a8d603616e6dd19d7873e3a5d0d6ef
zzzyoonnn <dwsrh03@gmail.com>
2024-06-05 11:45:31 +0900

    feat: 구현

--- Week11/나누어떨어지는배열/jiyoon.java
+++ Week11/나누어떨어지는배열/jiyoon.java
@@ -1,4 +1,29 @@
 package Week11.나누어떨어지는배열;
+import java.util.*;
 
 public class jiyoon {
+  class Solution {
+    public int[] solution(int[] arr, int divisor) {
+      int count = 0;
+
+      for (int i = 0; i < arr.length; i++) {
+        if (arr[i] % divisor == 0) count++;
+      }
+
+      if (count == 0) {
+        return new int[] {-1};
+      }
+
+      int index = 0;
+      int[] answer = new int[count];
+      for (int i = 0; i < arr.length; i++) {
+        if (arr[i] % divisor == 0) {
+          answer[index++] = arr[i];
+        }
+      }
+
+      Arrays.sort(answer);
+      return answer;
+    }
+  }
 }
--- Week11/문자열내마음대로정렬하기/jiyoon.java
+++ Week11/문자열내마음대로정렬하기/jiyoon.java
@@ -1,4 +1,26 @@
 package Week11.문자열내마음대로정렬하기;
+import java.util.*;
 
 public class jiyoon {
+  class Solution {
+    public String[] solution(String[] strings, int n) {
+      String[] answer = new String[strings.length];
+
+      ArrayList<String> list = new ArrayList<>();
+      for (int i = 0; i < strings.length; i++) {
+        // 맨 앞에 n번째 문자를 추가 후, 문자열 추가
+        list.add(strings[i].charAt(n) + strings[i]);
+      }
+
+      // list 정렬
+      Collections.sort(list);
+
+      for (int i = 0; i < answer.length; i++) {
+        // 맨 앞에 추가했던 문자를 제거 후, answer 배열에 삽입
+        answer[i] = list.get(i).substring(1);
+      }
+
+      return answer;
+    }
+  }
 }
