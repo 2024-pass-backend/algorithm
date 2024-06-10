1e617872c9342dada9f1bb49bc1b7980127cf675
zzzyoonnn <dwsrh03@gmail.com>
2024-06-10 10:51:14 +0900

    feat: 구현

--- Week12/_2016년/jiyoon.java
+++ Week12/_2016년/jiyoon.java
@@ -1,4 +1,19 @@
 package Week12._2016년;
 
 public class jiyoon {
+  class Solution {
+    public String solution(int a, int b) {
+      String answer = "";
+      String[] day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+      int[] month = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+
+      int days = 0;
+      for (int i = 0; i < a; i++) days += month[i];
+      days += b;
+
+      answer = day[(days + 4) % 7];
+      return answer;
+    }
+  }
 }
--- Week12/같은숫자는싫어/jiyoon.java
+++ Week12/같은숫자는싫어/jiyoon.java
@@ -1,4 +1,27 @@
 package Week12.같은숫자는싫어;
+import java.util.*;
 
 public class jiyoon {
+  public class Solution {
+    public int[] solution(int [] arr) {
+      LinkedList<Integer> list = new LinkedList<>();
+      list.add(arr[0]);
+
+      int pre = arr[0];
+      int now = 0;
+      for (int i = 1; i < arr.length; i++) {
+        now = arr[i];
+
+        if (pre != now) {
+          list.add(now);
+
+          pre = now;
+        }
+      }
+
+      // LinkedList를 Array로 변환
+      int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+      return answer;
+    }
+  }
 }
